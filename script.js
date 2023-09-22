fcb82e2c795a7dfe6847fbe0ecce4a3357ba4af0
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-23 00:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-//your JS code here. If required.
-
-// Find the target element by its id
 const targetElement = document.getElementById('level');
 
 // Initialize a variable to keep track of the DOM level
-let domLevel = 0;
-
-// Traverse the DOM tree upwards from the target element
-let currentElement = targetElement;
-while (currentElement && currentElement.tagName !== 'HTML') {
-    // Move to the parent element
-    currentElement = currentElement.parentElement;
+let domLevel = 1;
+ 
+while (true) {
     // If a parent element exists and it's not the root HTML element, increment the DOM level
-    if (currentElement && currentElement.tagName !== 'HTML') {
-        domLevel++;
+    if (targetElement.tagName === 'HTML') {
+        break;
     }
+    targetElement = targetElement.parentNode;
+    domLevel++;
 }
-
 // Display the DOM level using an alert
-alert(`The level of the element is: ${domLevel}`); 
+alert(`The level of the element is: ${Level}`); 
